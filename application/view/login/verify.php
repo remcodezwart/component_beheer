173664ad909a6154dc59f3610b9cc4b2f3224989
JurgenBV <Jurgen.vingerhoets@Hotmail.nl>
2017-05-16 13:56:26 +0200

    Translation verify.php

--- application/view/login/verify.php
+++ application/view/login/verify.php
@@ -1,12 +1,12 @@
 <div class="container">
 
-    <h1>Verification</h1>
+    <h1>Verificatie</h1>
     <div class="box">
 
         <!-- echo out the system feedback (error and success messages) -->
         <?php $this->renderFeedbackMessages(); ?>
 
-        <a href="<?php echo Config::get('URL'); ?>">Go back to home page</a>
+        <a href="<?=Config::get('URL'); ?>">Ga terug naar de hoofdpagina</a>
     </div>
 
 </div>
