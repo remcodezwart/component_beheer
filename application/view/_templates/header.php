80ae52435730f0d2a5b3add5ed5a2770c496725d
Remco de zwart <remcodezwart1997@gmail.com>
2017-05-23 13:33:23 +0200

    made menu responsive
    gave the menu a new look and its now responsive.
    also moved the style's from style2.css to style.css and removed
    style2.css

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -2,71 +2,65 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf_token" content="<?=Csrf::makeToken() ?>">
+    <meta name="url" content="<?=Config::get('URL')?>">
+
     <title>component beheer</title>
 
     <link rel="stylesheet" href="<?=Config::get('URL'); ?>css/style.css" />
     <link rel="stylesheet" href="<?=Config::get('URL'); ?>css/Spectre.css" />
-    <link rel="stylesheet" href="<?=Config::get('URL'); ?>css/style2.css" />
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf_token" content="<?=Csrf::makeToken() ?>">
-    <meta name="url" content="<?=Config::get('URL')?>">
 
     <!-- <script src="<?=Config::get('URL')?>js/initializing.js" type="text/javascript"></script>-->
 </head>
 <body>
     <div class="wrapper">
-        <!-- navigation -->
-        <nav class="navigation">
-            <li <?php if (View::checkForActiveController($filename, "index")) { echo ' class="active" '; } ?> >
-                <a href="<?=Config::get('URL'); ?>index/index">Home</a>
-            </li>
-            <li <?php if (View::checkForActiveController($filename, "overview")) { echo ' class="active" '; } ?> >
-                <a href="<?=Config::get('URL'); ?>profile/index">Profielen</a>
-            </li>
-            <?php if (Session::userIsLoggedIn()) { ?>
-                <li <?php if (View::checkForActiveController($filename, "dashboard")) { echo ' class="active" '; } ?> >
-                    <a href="<?=Config::get('URL'); ?>dashboard/index">Dashboard</a>
-                </li>
-                <li <?php if (View::checkForActiveController($filename, "Components")) { echo ' class="active" '; } ?> >
-                    <a href="<?=Config::get('URL'); ?>component/index">Componenten</a>
-                </li>
-            <?php } else { ?>
-                <!-- for not logged in users -->
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "login/index")) { echo ' class="active" '; } ?> >
-                    <a href="<?=Config::get('URL'); ?>login/index">Inloggen</a>
-                </li>
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "login/register")) { echo ' class="active" '; } ?> >
-                    <a href="<?=Config::get('URL'); ?>login/register">Registreren</a>
-                </li>
-            <?php } ?>
-        </nav>
-
-        <!-- my account -->
-        <nav class="navigation right">
-        <?php if (Session::userIsLoggedIn()) : ?>
-            <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                <a href="<?=Config::get('URL'); ?>login/showprofile">Mijn acount</a>
-                <ul class="navigation-submenu">
-                    <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                        <a href="<?=Config::get('URL'); ?>login/changeUserRole">verander acount type</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                        <a href="<?=Config::get('URL'); ?>login/editusername">wijzig gebruikersnaam</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                        <a href="<?=Config::get('URL'); ?>login/edituseremail">wijzig email-adress</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "location")) { echo ' class="active" '; } ?> >
-                        <a href="<?=Config::get('URL'); ?>location/index">locaties</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "supplier")) { echo ' class="active" '; } ?> >
-                        <a href="<?=Config::get('URL'); ?>supplier/index">Leveranciers</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                        <a href="<?=Config::get('URL'); ?>login/logout">Uitloggen</a>
-                    </li>
-                </ul>
-            </li>
-        <?php endif; ?>
-        </nav>
+        <div class="container">
+            <div class="columns">
+                <div class="center column col-12">
+                    <nav>
+                        <ul class="navigation">
+                            <li <?php if (View::checkForActiveController($filename, "index")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>index/index">Home</a>
+                            </li>
+                            <li <?php if (View::checkForActiveController($filename, "profile")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>profile/index">Profielen</a>
+                            </li>
+                            <?php if (Session::userIsLoggedIn()) { ?>
+                            <li <?php if (View::checkForActiveController($filename, "dashboard")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>dashboard/index">Dashboard</a>
+                            </li>
+                            <li <?php if (View::checkForActiveController($filename, "component")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>component/index">Componenten</a>
+                            </li>
+                            <li <?php if (View::checkForActiveControllerAndAction($filename,                   "login/changeUserRole")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>login/changeUserRole">verander acount type</a>
+                            </li>
+                            <li <?php if (View::checkForActiveControllerAndAction($filename,                   "login/editUsername")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>login/editusername">wijzig gebruikersnaam</a>
+                            </li>
+                            <li <?php if (View::checkForActiveControllerAndAction  ($filename,                 "login/editUserEmail")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>login/edituseremail">wijzig email-adress</a>
+                            </li>
+                            <li <?php if (View::checkForActiveController($filename, "location")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>location/index">locaties</a>
+                            </li>
+                            <li <?php if (View::checkForActiveController($filename, "supplier")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>supplier/index">Leveranciers</a>
+                            </li>
+                            <li <?php if (View::checkForActiveAction($filename, "login")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>login/logout">Uitloggen</a>
+                            </li>
+                            <?php } else { ?>
+                            <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>login/index">Inloggen</a>
+                            </li>
+                            <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
+                                <a href="<?=Config::get('URL'); ?>login/register">Registreren</a>
+                            </li>
+                            <?php } ?>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
