e8f5a3b77015dee3ac8e963a8b628f52fc99145e
Remco de zwart <remcodezwart1997@gmail.com>
2017-05-23 14:07:33 +0200

    added messages for when there are no parts

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -1,40 +1,34 @@
-
 <div class="container">
-    <h1>IndexController/index</h1>
+    <h1>Onderdelen</h1>
     <div class="box">
 
         <?=$this->renderFeedbackMessages()?>
-
-        <h3>What happens here ?</h3>
-        <p>
-            This is the homepage. As no real URL-route (like /login/register) is provided, the app uses the default
-            controller and the default action, defined in application/config/config.php, by default it's
-            IndexController and index()-method. So, the app will load application/controller/IndexController.php and
-            run index() from that file. Easy. That index()-method (= the action) has just one line of code inside
-            ($this->view->render('index/index');) that loads application/view/index/index.php, which is basically
-            this text you are reading right now.
-        </p>
-        <?php foreach($this->component as $key => $value) {?>
-            <div class="col-md-8">
-                <h2><?=$value->name?></h2>
-                <p><?=$value->hyperlink?></p>
-                <p><?=$value->description?> <?=$value->specs?></p>
-                <p>In voorraad: <?=$value->amount?></p>
-                <?php if (Session::userIsLoggedIn()) : ?>
-                    <form method="post" action="<?=Config::get('URL'); ?>component/editSave">
-                        <p>Verander beschrijving:<input type="textarea" name="description" value="<?=$value->description?>"/></p>
-                        <p>Verander specs:<input type="textarea" name="specs" value="<?=$value->specs?>"/></p>
-                        <p>Verander hyperlink:<input type="textarea" name="hyperlink" value="<?=$value->hyperlink?>"/></p>
-                        <p>Verander voorraad:<input type="textarea" name="amount" value="<?=$value->amount?>"/></p>
-                        <input type="hidden" name="id" value="<?=$value->id?>"/>
-                        <input type="submit" class="button" value='Sla op'/>
-                    </form>
-                    <form method="post" action="<?=Config::get('URL'); ?>component/delete">
-                        <input type="hidden" name="id" value="<?=$value->id?>"/>
-                        <input type="submit" class="button" value='verwijder'/>
-                    </form>
-                <?php endif; ?>
-            </div>
-        <?php } ?>
+        
+        <?php foreach($this->component as $value): ?>
+            <h2>naam: <?=$value->name?></h2>
+            <img src="<?=$value->hyperlink?>" alt="component plaatje">
+            <p>beschrijving: <?=$value->description?></p>
+            <pre>specs: <?=$value->specs?></pre>
+            <p>In voorraad: <?=$value->amount?></p>
+            <?php if (Session::userIsLoggedIn()) : ?>
+                <form method="post" action="<?=Config::get('URL'); ?>component/editSave">
+                    <p>Verander beschrijving:</p>
+                    <textarea name="description"><?=$value->description ?></textarea>
+                    <p>Verander specs:</p>
+                    <textarea name="specs"><?=$value->specs ?></textarea>
+                    <p>Verander hyperlink:<input type="text" name="hyperlink" value="<?=$value->hyperlink?>"/></p>
+                    <p>Verander aantal in de voorraad:<input type="textarea" name="amount" value="<?=$value->amount?>"/></p>
+                    <input type="hidden" name="id" value="<?=$value->id?>"/>
+                    <button type="submit">Sla op</button>
+                </form>
+                <form method="post" action="<?=Config::get('URL'); ?>component/delete">
+                    <input type="hidden" name="id" value="<?=$value->id?>"/>
+                    <button type="submit">verwijder</button>
+                </form>
+            <?php endif; ?>
+        <?php endforeach; ?>
+        <?php if (!$this->component): ?>
+            <p>er zijn nog geen onderdelen</p>
+        <?php endif; ?>
     </div>
 </div>
