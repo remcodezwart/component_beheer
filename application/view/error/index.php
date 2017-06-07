4ad562cc8ae86521e615f25a2919ac374a9484a1
remcodezwart <remcodezwart1997@gmail.com>
2017-06-07 14:45:11 +0200

    missed a page

--- application/view/error/index.php
+++ application/view/error/index.php
@@ -1,6 +1,3 @@
-<div class="container">
-    <h1>Page not found</h1>
-    <div class="box">
-        <p class="red-text">This page does not exist.</p>
-    </div>
-</div>
+<h1>Page not found</h1>
+
+<p class="red-text">This page does not exist.</p>
