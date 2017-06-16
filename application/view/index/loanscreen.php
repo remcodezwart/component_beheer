49d36726dba1f7b520da78b6db62cb1053dc2825
Remco de zwart <remcodezwart1997@gmail.com>
2017-06-16 09:01:44 +0200

    implemented barcode fix

--- application/view/index/loanscreen.php
+++ application/view/index/loanscreen.php
@@ -1,7 +1,8 @@
+<?php if ($this->component) { ?>
 <h2><?=$this->component->name?></h2>
-<p><img alt="component plaatje" src="<?=$this->component->hyperlink?>"</p>
+<p><img alt="component plaatje" src="<?=$this->component->hyperlink?>"></p>
 <p><?=$this->component->description?></p>
-<p><pre><?=$this->component->specs?></pre></p>
+<pre><?=$this->component->specs?></pre>
 <p>In voorraad: <?=$this->component->amount?></p>
 <p>Je kan op dit moment alleen lenen van Duurzaamheidsfabriek Dordrecht Da Vinci.</p>
 <br>
@@ -10,8 +11,11 @@
     <input type="hidden" name="id" value="<?=$this->component->id?>"/>
     <input type="hidden" name="csrf_token" value="<?= Csrf::makeToken(); ?>">
     <p>Mijn barcode is...</p>
-    <input type="text" name="barcode" required="true"/><br>
+    <input id="barcode" type="text" name="barcode" required="true"/><br>
 	<button class="btn waves-effect waves-light blue" type="submit" name="action">uitlenen
 		<i class="material-icons right">send</i>
 	</button>
 </form>
+<?php } else { ?>
+<p class="red align-center"></p>
+<?php } ?>
--- public/js/barcodeFix.js
+++ public/js/barcodeFix.js
@@ -1,4 +1,13 @@
 document.addEventListener('keydown', function(event) {
-  if( event.keyCode == 13 || event.keyCode == 17 || event.keyCode == 74 )
-    event.preventDefault();
+  	
+  	if (event.target.id === 'search') {
+		if (event.keyCode == 13 || event.keyCode == 17 || event.keyCode == 74 ) {
+	   		event.preventDefault();
+		}
+
+		if (event.keyCode == 74) {
+			document.getElementById('search').value += "j";
+		}
+  	}
+
 });
